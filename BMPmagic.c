f9ddde9178c87abeba286960fe1709e51226bae3
Sebbe <12002498@student.pxl.be>
2022-04-24 16:50:19 +0200

    outputfile als comment. Functie voor header uitlezen
    afbeelding zonder padding kan ook uitgelezen

--- BMPmagic.c
+++ BMPmagic.c
@@ -7,19 +7,38 @@
 
 #define OUTPUT "output.BMP"
 
+void HeaderLezen(FILE *file, char* Head, int *h, int *b );
+
 int main(int argc, char *argv[])
 {
+	char *bmpHeader = "";
+    char *image = NULL; 
+	int *breedte = NULL;
+    int *hoogte = NULL;
+	int padding = 0;
+	int imagesize = 0;
+	
+	
+	
+	
+	
+	FILE *path = NULL;
 	if (argv[1]=="--help" || argv[1]== "--h")
 	{
 		/* code TO DO MIGUEL*/ 
 	}
 	else
 	{
-		FILE *path = fopen(argv[1], "r");
+		path = fopen(argv[1], "r");
 	}
 	
+	HeaderLezen(path, bmpHeader, hoogte, breedte);
+	padding = *breedte %4;
+	imagesize = *hoogte * *breedte;
+	image = (char *) malloc(imagesize);
+	
 
-	FILE *newf = fopen(OUTPUT,"w+");
+	//FILE *newf = fopen(OUTPUT,"wb+");
 	
 	if(path == NULL) //Test of het open van de file gelukt is!
     {
@@ -27,22 +46,48 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 	
-	if(newf == NULL) //Test of het open van de file gelukt is!
+	/*if(newf == NULL) //Test of het open van de file gelukt is!
     {
         printf("Something went wrong while trying to create %s\n", newf);
         exit(EXIT_FAILURE);
-    }
+    }*/
 	
-	char bmpHeader[54]; // voorzie een array van 54-bytes voor de BMP Header
-    fread(bmpHeader, sizeof(unsigned char), 54, path); // lees de 54-bytes header
+		
+	//skip paddings
+	if(padding)
+	{
+		/*code TO DO MIGUEL*/
+	}
+	//lees alles
+	else
+	{
+		fread(image, sizeof(unsigned char), imagesize, path);
+	}
+	
+	
+	fclose(path);
+	/*if(newf != NULL)
+	{
+		fclose(newf);
+	}*/
+	
+	
+	return 0;
+}
 
-    // haal de filetype, hoogte en breedte uit de header
+void HeaderLezen(FILE *file, char* Head, int *h, int *b)
+{
+	//header inlezen
+	fread(Head, sizeof(unsigned char), 54, file);
+	
+	// haal de filetype, hoogte en breedte uit de header
+	
 	char filetype[2]="";
-    int breedte = *(int*)&bmpHeader[18];
-    int hoogte = *(int*)&bmpHeader[22];
-	int bitformat = *(int*)&bmpHeader[28];
-	int padding = (breedte*hoogte)%4;
-	strncpy(filetype, bmpHeader, 2);
+    b = (int*)&Head[18];
+    h = (int*)&Head[22];
+	int bitformat = *(int*)&Head[28];
+	
+	strncpy(filetype, Head, 2);
 	
 	if(strncmp(filetype, "BM",2)!=0)
 	{
@@ -54,20 +99,10 @@ int main(int argc, char *argv[])
 		printf("bitmapfile is niet 24 bits\n");
 	}
 	
-	//skip paddings
-	if(padding)
-	{
-		
-	}
-	//lees alles
-	else
-	{
-		
-	}
-	
-	
-	fclose(path);
-	fclose(newf);
-	
-	return 0;
 }
+
+
+
+
+
+
